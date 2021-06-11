267aaea66381a4d10ea9a6c5a4b43f084b785d6a
Elie Schoppik <eschoppik@gmail.com>
2021-06-11 07:25:30 -0700

    heroku ssl prep

--- db.js
+++ db.js
@@ -5,7 +5,10 @@ const { Client } = require("pg");
 let DB_URI = "postgresql:///users-messages-gql";
 
 let db = new Client({
-  connectionString: process.env.DATABASE_URL || DB_URI
+  connectionString: process.env.DATABASE_URL || DB_URI,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 db.connect();
